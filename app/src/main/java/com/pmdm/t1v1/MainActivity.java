f1b0cb380b7284c73526bd5535ca60db4d8c0765
dominp97 <36171336+dominp97@users.noreply.github.com>
2018-11-20 11:39:46 +0100

    Toolbar cambiada, y switch puesto

--- app/src/main/java/com/pmdm/t1v1/MainActivity.java
+++ app/src/main/java/com/pmdm/t1v1/MainActivity.java
@@ -2,12 +2,12 @@ package com.pmdm.t1v1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
